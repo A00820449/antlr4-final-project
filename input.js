ae0ef6949eaacd24aff5ae9c8c521dbfc9244542
Miguel Tornero <a00820449@itesm.mx>
2023-04-24 17:21:40 -0600

    added input function generator

---
+++ input.js
@@ -0,0 +1,62 @@
+import { createInterface } from "node:readline/promises"
+
+/**
+ * @typedef {(prompt?: string) => Promise<string>} InputFunction
+ */
+
+/**
+ * @param {ReadableStream} stream 
+ */
+export async function getBufferFromStream(stream) {
+    /**
+     * @type {Buffer[]}
+     */
+    const buffer = []
+    for await (const data of stream) {
+        if (data instanceof Buffer) {
+            buffer.push(data)
+        }   
+    }
+    return Buffer.concat(buffer)
+}
+
+/**
+ * @param {NodeJS.ReadStream} input 
+ * @param {NodeJS.WriteStream} output 
+ * @returns {InputFunction}
+ */
+export function getTTYInputFunction(input, output) {
+    return async (prompt) => {
+        const rl = createInterface({input, output})
+        const a = await rl.question(prompt)
+        rl.close()
+        return a
+    }
+}
+
+/**
+ * @param {Buffer} buffer 
+ * @param {NodeJS.WriteStream} output 
+ * @return {InputFunction}
+ */
+export function getNonTTYFunction(buffer, output) {
+    const queue = buffer.toString().replace(/\n$/,"").split('\n')
+    
+    return async (prompt) => {
+        output.write(prompt)
+        return queue.shift() || ''
+    }
+}
+
+/**
+ * 
+ * @param {NodeJS.ReadStream} input 
+ * @param {NodeJS.WriteStream} output 
+ */
+export async function getInputFuntion(input, output) {
+    if (input.isTTY) {
+        return getTTYInputFunction(input, output)
+    }
+    const buffer = await getBufferFromStream(input)
+    return getNonTTYFunction(buffer, output)
+}
